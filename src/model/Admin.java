b145903ecaf58dfbf8c5121fd7399504c6ea9a82
Mladen Samardžić <szmlad@gmail.com>
2021-01-23 16:56:47 +0100

    Add constructors to Admin class.

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -1,5 +1,13 @@
 package model;
 
-public class Admin extends User {
+import java.time.LocalDate;
 
+public class Admin extends User {
+    public Admin() {
+        super();
+    }
+
+    public Admin(String username, String password, String firstName, String lastName, Gender gender, LocalDate birthDate) {
+        super(username, password, firstName, lastName, gender, birthDate);
+    }
 }
