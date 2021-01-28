b14b66f0ddd07275fe4b8a7adc084a04809808a0
Mladen Samardžić <szmlad@gmail.com>
2021-01-28 13:09:43 +0100

    Add margin to the last elemnt of the manifestation list so that its card shadow is visible

--- static/js/manifList.js
+++ static/js/manifList.js
@@ -7,7 +7,7 @@ Vue.component('manif-list', {
     },
     template: `
 <div id="list-manifestations">
-    <div v-for="(m, i) in manifestations">
+    <div v-for="(m, i) in manifestations" class="list-manifs">
         <manif-card v-bind:manifestation="m" v-bind:isHidden="m.isHidden"></manif-card>
     </div>
 </div>
